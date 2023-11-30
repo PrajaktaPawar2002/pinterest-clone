030690f4eb540edd5510e34cf4bc6067b173e18f
TanujaShekhawat <tanuja0363@gmail.com>
2023-11-30 07:10:13 +0000

    first commit

---
+++ newapp/routes/users.js
@@ -0,0 +1,52 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+
+
+const mongoose = require('mongoose');
+
+const plm = require("passport-local-mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/newapp");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  dp: {
+    type: String, // You might want to store the URL or file path to the user's profile picture
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
+
+
+
+
+//59.41
